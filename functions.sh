7d8195bc277e7b60018b531053ab3a6a039c9bbc
tazeek <tazeek.rakib@gmail.com>
2021-01-25 23:16:20 +0800

    New alias added in function

--- functions.sh
+++ functions.sh
@@ -6,3 +6,11 @@ function new_pr() {
 	git commit --allow-empty -m "$branch_name"
 	git push origin $branch_name
 }
+
+function new_commit() {
+	commit_message=$1
+	branch_name=git rev-parse --abbrev-ref HEAD
+
+	git commit -m "$1"
+	git push origin $branch_name
+}
