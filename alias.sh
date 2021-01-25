34aa71523a2f39cab14fb6bb64ec9a3ec9075b77
tazeek <tazeek.rakib@gmail.com>
2021-01-25 23:14:53 +0800

    Alias names updated

--- alias.sh
+++ alias.sh
@@ -1,7 +1,7 @@
-alias delete-merged='git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -d'
+alias delete_merged='git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -d'
 
-alias home-base='cd ~/Desktop/Projects'
+alias project_file='cd ~/Desktop/Projects'
 
-alias master='git checkout'
+alias main_branch='git checkout'
 
 alias update='git pull'
